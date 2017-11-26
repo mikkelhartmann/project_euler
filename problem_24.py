71d63db60e545694002f1c2d50feaa87c7b60d6d
Mikkel Hartmann Jensen <mikkelheart@gmail.com>
2017-11-26 21:25:06 +0100

    Two more.

--- problem_24.py
+++ problem_24.py
@@ -7,3 +7,9 @@ we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are
 
 What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 """
+import itertools
+
+digits = [0,1,2,3,4,5,6,7,8,9]
+permutations = list(itertools.permutations(digits, len(digits)))
+print(len(permutations))
+print(sorted(permutations)[999999])
